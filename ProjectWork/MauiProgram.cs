ee87fb08f9a0590bc4614f5e62cb7038e5140c4d
angelo.bonicelli <angelo.bonicelli@gmail.com>
2022-12-21 15:24:12 +0100

    Added community toolkit as a depdendency

--- ProjectWork/MauiProgram.cs
+++ ProjectWork/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using ProjectWork.Data;
 
 namespace ProjectWork;
@@ -10,7 +11,8 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
