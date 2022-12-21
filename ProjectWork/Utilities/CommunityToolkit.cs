57e69f98593d856ca51809b0555e6e622c1eb6b4
Aiuola <99275203+Aiuola@users.noreply.github.com>
2022-12-21 15:45:16 +0100

    3 add maui community toolkit (#4)
    * Added community toolkit as a depdendency
    
    * Added a Utilities class

---
+++ ProjectWork/Utilities/CommunityToolkit.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWork.Utilities
+{
+    internal class CommunityToolkit
+    {
+        public static async Task CreateToast(string text,
+            ToastDuration duration = ToastDuration.Short, 
+            double fontSize = 14)
+        {
+            CancellationTokenSource cancellationTokenSource = new();
+
+            var toast = Toast.Make(text, duration, fontSize);
+
+            await toast.Show(cancellationTokenSource.Token);
+        }
+    }
+}
