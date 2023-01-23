1fd46668279b43746f0b7da8b5b842a8afbe21ed
Kevin Sinchi <83868719+RevenBot@users.noreply.github.com>
2023-01-23 16:56:13 +0100

    13 dependencies project strucutre (#14)
    * New Structure
    
    * Movie as example
    
    * Add  README structure

---
+++ ProjectWork/Data/ViewModels/IViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectWork.Data.ViewModels
+{
+    public interface IViewModel<T>
+    {
+        List<T> Items { get; set; }
+
+        Task CreateItem(T item);
+        Task ReadItems();
+        Task UpdateItem(T item);
+        Task DeleteItem(T item);
+
+    }
+}
