1059fcf09738000768439fc1981811f5cf253daf
SystemaWolf <x99daniele99x@gmail.com>
2023-01-28 14:43:48 +0100

    Fixed Compatibility with Android

---
+++ ProjectWork/Models/LoginAgent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWork.Models
+{
+    internal class LoginAgent
+    {
+
+        public LoginAgent() {
+           
+        }
+        internal bool sendRequest()
+        {
+            //TODO send request to Django
+            return true;
+        }
+
+        public bool validateLogin() {
+            sendRequest();
+
+            //TODO Compare the credential and login if true
+            return true; 
+        }
+    }
+}
