494d5e128cfd6f25c7c7704a86b660b07fe5edec
SystemaWolf <x99daniele99x@gmail.com>
2023-02-13 19:53:31 +0100

    Prapared the UI for the admin dashboard.

---
+++ ProjectWork/Data/Services/LoginAgent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWork.Data.Services
+{
+
+    public static class LoginAgent
+    {
+        private static bool loggedIn = false;
+
+        static LoginAgent() { }
+
+        public static bool isLoggedIn()
+        {
+            //TODO check the Token
+            return loggedIn;
+        }
+
+        public static void setLoggedIn(bool b)
+        {
+            loggedIn = b;
+        }
+
+    }
+}
--- ProjectWork/MauiProgram.cs
+++ ProjectWork/MauiProgram.cs
@@ -1,34 +1,64 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
 using ProjectWork.Data;
+using ProjectWork.Data.Services;
 using ProjectWork.Data.ViewModels;
+using Microsoft.Maui.LifecycleEvents;
 
 namespace ProjectWork;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
+
+
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
             .UseMauiCommunityToolkit()
             .ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-		builder.Services.AddMauiBlazorWebView();
+
+
+
+        builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
 #endif
 
-		builder.Services.AddSingleton<WeatherForecastService>();
-		builder.Services.AddSingleton<ArtworkViewModel>();
-		builder.Services.AddSingleton<AccountViewModel>();
+        builder.Services.AddSingleton<ArtworkViewModel>();
+        builder.Services.AddSingleton<AccountViewModel>();
+
+#if WINDOWS
+                builder.ConfigureLifecycleEvents(events =>
+                {
+                 
+                    events.AddWindows(windowsLifecycleBuilder =>
+                    {
+                        windowsLifecycleBuilder.OnWindowCreated(window =>
+                        {
+                            window.ExtendsContentIntoTitleBar = true;
+                            var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                            var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+                            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+                            switch (appWindow.Presenter)
+                            {
+                                case Microsoft.UI.Windowing.OverlappedPresenter overlappedPresenter:
+                                    overlappedPresenter.SetBorderAndTitleBar(true, true);
+                                    overlappedPresenter.Maximize();
+                                    break;
+                            }
+                        });
+                    });
+                });
+#endif
 
         return builder.Build();
-	}
+    }
 }
