5b2b06a48bb659b89a7051b3a0d0226f481826c2
Kevin <kevinjss97@gmail.com>
2023-03-01 17:08:09 +0100

    Adapt with new model and fix Paginator

--- ProjectWork/Models/Artwork/BaseArtwork.cs
+++ ProjectWork/Models/Artwork/BaseArtwork.cs
@@ -5,21 +5,32 @@ namespace ProjectWork.Models.Artwork
 {
     public interface IArtWorkBase
     {
-        [JsonPropertyName("artist")]
-        public string Artist { get; set; }
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+
+        [JsonPropertyName("author_id")]
+        public int AuthorId { get; set; }
 
         [JsonPropertyName("title")]
         public string Title { get; set; }
 
-        public string PictureData { get; set; }
-
-        public string FileInfo { get; set; }
-
+        [JsonPropertyName("Date")]
         public string Date { get; set; }
 
-        public string Type { get; set; }
+        [JsonPropertyName("tecnique")]
+        public string Technique { get; set; }
 
-        public string Size { get; set; }
+        [JsonPropertyName("location")]
+        public string Location { get; set; }
+
+        [JsonPropertyName("form")]
+        public string Form { get; set; }
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+        [JsonPropertyName("timeframe")]
+        public string TimeFrame { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
 
     }
     public interface IDownloadArtwork : IArtWorkBase
@@ -41,29 +52,36 @@ namespace ProjectWork.Models.Artwork
         public int Id { get; set; }
         [JsonPropertyName("image_url")]
         public string ImageUrl { get; set; }
-        [JsonPropertyName("artist")]
-        public string Artist { get; set; }
+
+
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+
+        [JsonPropertyName("author_id")]
+        public int AuthorId { get; set; }
 
         [JsonPropertyName("title")]
         public string Title { get; set; }
 
-        [JsonPropertyName("picture_data")]
-        public string PictureData { get; set; }
-
-        [JsonPropertyName("file_info")]
-        public string FileInfo { get; set; }
-
-        [JsonPropertyName("date")]
+        [JsonPropertyName("Date")]
         public string Date { get; set; }
 
+        [JsonPropertyName("tecnique")]
+        public string Technique { get; set; }
+
+        [JsonPropertyName("location")]
+        public string Location { get; set; }
+
+        [JsonPropertyName("form")]
+        public string Form { get; set; }
         [JsonPropertyName("type")]
         public string Type { get; set; }
+        [JsonPropertyName("timeframe")]
+        public string TimeFrame { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
 
-        [JsonPropertyName("size")]
-        public string Size { get; set; }
-
-        [JsonPropertyName("museum")]
-        public string Museum { get; set; }
+        [JsonPropertyName("file")]
         [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
         public IBrowserFile File { get; set; }
     }
