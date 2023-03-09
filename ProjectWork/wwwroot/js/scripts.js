a54c6c7e67f050d29ad1e90cd5026a3cfcb3ba5f
SystemaWolf <x99daniele99x@gmail.com>
2023-03-09 17:30:58 +0100

    Added JS script

---
+++ ProjectWork/wwwroot/js/scripts.js
@@ -0,0 +1,6 @@
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
