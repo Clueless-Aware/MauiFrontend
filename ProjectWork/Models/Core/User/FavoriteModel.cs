16ec2f9e44023134d3f3534f45180ca1235c491d
Aiuola <angelo.bonicelli@fitstic-edu.com>
2023-03-10 12:08:08 +0100

    Removed old user class

--- ProjectWork/Models/Core/User/FavoriteModel.cs
+++ ProjectWork/Models/Core/User/FavoriteModel.cs
@@ -5,5 +5,5 @@ namespace ProjectWork.Models.Core.User;
 public class FavoriteModel
 {
     [JsonPropertyName("user")] public int UserId { get; set; }
-    [JsonPropertyName("artwork")] private int ArtworkId { get; set; }
+    [JsonPropertyName("artwork")] public int ArtworkId { get; set; }
 }
