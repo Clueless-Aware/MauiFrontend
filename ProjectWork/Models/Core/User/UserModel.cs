659948858c85d813c0d55bc328cbce165fb2f520
Aiuola <angelo.bonicelli@fitstic-edu.com>
2023-03-12 15:32:46 +0100

    Bookmark is now a list of artworks

--- ProjectWork/Models/Core/User/UserModel.cs
+++ ProjectWork/Models/Core/User/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿#nullable enable
 using System.Text.Json.Serialization;
+using ProjectWork.Models.Artwork;
 
 namespace ProjectWork.Models.Core.User;
 
@@ -7,23 +8,26 @@ public class UserModel
 {
     [JsonPropertyName("id")] public int Id { get; set; }
 
-    [JsonPropertyName("favorite_artist")] public int FavoriteArtist { get; set; }
+    [JsonPropertyName("favorite_artist")] public int? FavoriteArtist { get; set; }
 
-    [JsonPropertyName("profile_picture")] public string? ProfilePicture { get; set; }
+    [JsonPropertyName("profile_picture")] public string ProfilePicture { get; set; }
 
     [JsonPropertyName("last_login")] public DateTime LastLogin { get; set; }
 
-    [JsonPropertyName("username")] public string? Username { get; set; }
+    [JsonPropertyName("username")] public string Username { get; set; }
 
     [JsonPropertyName("first_name")] public string? FirstName { get; set; }
 
     [JsonPropertyName("last_name")] public string? LastName { get; set; }
 
-    [JsonPropertyName("email")] public string? Email { get; set; }
+    [JsonPropertyName("email")] public string Email { get; set; }
 
     [JsonPropertyName("is_staff")] public bool IsStaff { get; set; }
 
-    [JsonPropertyName("date_joined")] public DateTime? DateJoined { get; set; }
+    [JsonPropertyName("date_joined")] public DateTime DateJoined { get; set; }
 
     [JsonPropertyName("biography")] public string? Biography { get; set; }
+
+    [JsonPropertyName("bookmarked_artworks")]
+    public List<BaseArtwork> BookmarkedArtworks { get; set; }
 }
