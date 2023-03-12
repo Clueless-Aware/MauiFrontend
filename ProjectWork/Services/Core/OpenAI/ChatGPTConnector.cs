97ce7c9688eca785482b7bc763aaf262e7f37a7d
SystemaWolf <x99daniele99x@gmail.com>
2023-03-12 21:05:58 +0100

    ChatGPT done.

---
+++ ProjectWork/Services/Core/OpenAI/ChatGPTConnector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWork.Services.Core.OpenAI
+{
+    internal class ChatGPTConnector
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public ChatGPTConnector(string apiKey)
+        {
+            _apiKey = apiKey;
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://api.openai.com/v1/");
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+        }
+
+        public async Task<string> SendPrompt(string prompt, string model)
+        {
+            var requestBody = new
+            {
+                prompt = prompt,
+                model = model,
+                max_tokens = 4096,
+                temperature = 0.5
+            };
+
+            var response = await _httpClient.PostAsJsonAsync("completions", requestBody);
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            return responseBody;
+        }
+    }
+}
