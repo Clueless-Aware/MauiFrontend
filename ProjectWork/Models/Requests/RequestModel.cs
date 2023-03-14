cf8b85461ce8e20fd1e239cee1e3fe51a264448b
Aiuola <angelo.bonicelli@fitstic-edu.com>
2023-03-14 22:31:44 +0100

    Requests models

---
+++ ProjectWork/Models/Requests/RequestModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProjectWork.Models.Requests;
+
+public class RequestModel
+{
+    [JsonPropertyName("content")] private string Content { get; set; }
+    [JsonPropertyName("date")] private DateOnly Date { get; set; }
+    [JsonPropertyName("critical")] private bool IsCritical { get; set; }
+    [JsonPropertyName("seen")] private bool IsSeen { get; set; }
+    [JsonPropertyName("subject")] private string Subject { get; set; }
+
+    [JsonPropertyName("from_user")] private int UserId { get; set; }
+    //[JsonPropertyName("user_details")] private int UserModel { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Content} - {Subject} - {Date}";
+    }
+}
---
+++ ProjectWork/Models/Requests/RequestUploadModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProjectWork.Models.Requests;
+
+public class RequestUploadModel
+{
+    [JsonPropertyName("content")] private string Content { get; set; }
+    [JsonPropertyName("critical")] private bool IsCritical { get; set; }
+    [JsonPropertyName("subject")] private string Subject { get; set; }
+    [JsonPropertyName("from_user")] private int UserId { get; set; }
+}
